138b00c89b8b2c507abc4d904023d58d53f7dab7
5sudeng <sudeng@g.skku.edu>
2024-11-11 21:59:52 +0900

    add ex

--- 1주차/1, 2, 3 더하기/1, 2, 3 더하기.cpp
+++ 1주차/1, 2, 3 더하기/1, 2, 3 더하기.cpp
@@ -3,6 +3,11 @@ Dynamic programming!
 문제 설명은 readme 참고..
 base case : n=1, n=2, n=3 (1,2,3을 더해서 만들어야 하는 상황..)
 점화식 -> n의 경우의수 = n-1일 때 + n-2일 때 + n-3일 때..
+
+4
+arr[1] -> 3
+arr[2] -> 2
+arr[3] -> 1
 */
 #include <iostream>
 using namespace std;
