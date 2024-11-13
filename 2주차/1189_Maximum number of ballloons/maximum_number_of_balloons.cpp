32ca13b3bb2d084119e2fdc0e9da5b5d45904722
5sudeng <sudeng@g.skku.edu>
2024-11-13 11:54:10 +0900

    ballon

--- 2주차/1189_Maximum number of ballloons/maximum_number_of_balloons.cpp
+++ 2주차/1189_Maximum number of ballloons/maximum_number_of_balloons.cpp
@@ -6,7 +6,7 @@ public:
     int maxNumberOfBalloons(string text) {
         double cnt[] = {0,0,0,0,0};
         double result = 100000;
-        char eng[] = {'b', 'a', 'l', 'o', 'n'};
+        char eng[] = {'b', 'a', 'l', 'o', 'n'}; //ba ll oo n
         double values[] = {1, 1, 0.5, 0.5, 1};
         for (auto val: text) {
             for (int i=0; i<5; i++) {
