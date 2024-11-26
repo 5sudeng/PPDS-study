3deb76308cfcefa06b93604bc5e3925daf20a643
5sudeng <sudeng@g.skku.edu>
2024-11-26 16:34:38 +0900

    //

--- 6회차/225_Implement_Stack_using_Queues/implement_stack_using_queue.cpp
+++ 6회차/225_Implement_Stack_using_Queues/implement_stack_using_queue.cpp
@@ -3,31 +3,31 @@ using namespace std;
 
 class MyStack {
 private: 
-    queue<int> q1, tmp;
+    queue<int> q1, tmp; //queue 두개로 구현 - q1이 메인..
 public:
     MyStack() {
     }
     
-    void push(int x) {
+    void push(int x) { //push는 그냥 push..
         q1.push(x);
     }
     
-    int pop() {
+    int pop() { //pop이 관건 - q1이 하나 남을 때까지 (stack 기준 top만 남을 때까지) pop하면서 임시 큐에 넣어준다.
         int back = q1.back();
         while (q1.size() > 1) {
             tmp.push(q1.front());
             q1.pop();
         }
-        q1.pop();
-        swap(q1, tmp);
+        q1.pop(); //q1을 마저 비우고... (초기상태가됨)
+        swap(q1, tmp); //두개 바꿔주면..
         return back;
     }
     
-    int top() {
+    int top() { //top은 queue의 back으로..
         return q1.back();
     }
     
-    bool empty() {
+    bool empty() { 
         return q1.empty();
     }
 };
